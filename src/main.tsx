b1768d858e4efd20825d1a0e5bc320ecc5fe1edc
JonahJL <55671274+JonahJL@users.noreply.github.com>
2026-03-12 14:10:38 -0400

    main.tex debug print

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import AppRouter from './AppRouter';
 
+console.log('main.tsx loaded');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppRouter />
